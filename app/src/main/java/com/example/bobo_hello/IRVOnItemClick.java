470a6b5364acf81aaca07f6ab81cd2655e45ea4f
kc.bezshlyakh <kc_bezshlyiakh@gmail.com>
2020-08-24 13:55:41 +0300

    Ex6 - recycler view for cities menu

---
+++ app/src/main/java/com/example/bobo_hello/IRVOnItemClick.java
@@ -0,0 +1,5 @@
+package com.example.bobo_hello;
+
+public interface IRVOnItemClick {
+    void onItemClicked(String itemText, int position);
+}
--- app/src/main/java/com/example/bobo_hello/fragments/CitiesFragment.java
+++ app/src/main/java/com/example/bobo_hello/fragments/CitiesFragment.java
@@ -6,28 +6,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
-import com.example.bobo_hello.MainActivity;
+import com.example.bobo_hello.IRVOnItemClick;
 import com.example.bobo_hello.OptionsContainer;
 import com.example.bobo_hello.R;
+import com.example.bobo_hello.RecyclerDataAdapter;
 import com.example.bobo_hello.WeatherActivity;
 import com.example.bobo_hello.WeatherInfoContainer;
 
-public class CitiesFragment extends Fragment {
-
-    private ListView listView;
-    private TextView emptyTextView;
+public class CitiesFragment extends Fragment implements IRVOnItemClick {
+    private RecyclerView recyclerView;
     private boolean tempOn = true, windOn = true;
     private boolean isExistWeatherDisp;
     private int currentPosition = 0;
@@ -42,7 +43,7 @@ public class CitiesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
-        initList();
+        setupCitiesRecycler();
     }
 
     @Override
@@ -55,7 +56,6 @@ public class CitiesFragment extends Fragment {
             currentPosition = savedInstanceState.getInt("CurrentCity", 0);
         }
         if (isExistWeatherDisp) {
-            listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
             showWeatherDisp();
         }
     }
@@ -72,30 +72,19 @@ public class CitiesFragment extends Fragment {
     }
 
     private void initViews(View view) {
-        listView = view.findViewById(R.id.listOfCities);
-        emptyTextView = view.findViewById(R.id.cities_list_empty_view);
+        recyclerView = view.findViewById(R.id.cities_menu);
     }
 
-    private void initList() {
-        ArrayAdapter<CharSequence> adapter =
-                ArrayAdapter.createFromResource(Objects.requireNonNull(getActivity()), R.array.cities,
-                        android.R.layout.simple_list_item_activated_1);
-        listView.setAdapter(adapter);
-
-        listView.setEmptyView(emptyTextView);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                currentPosition = position;
-                showWeatherDisp();
-            }
-        });
+    private void setupCitiesRecycler() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        ArrayList<String> listData = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.cities)));
+        RecyclerDataAdapter adapter = new RecyclerDataAdapter(listData, this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 
     private void showWeatherDisp() {
         if (isExistWeatherDisp) {
-            listView.setItemChecked(currentPosition, true);
             WeatherInfoFragment detail = (WeatherInfoFragment)
                     Objects.requireNonNull(getFragmentManager()).findFragmentById(R.id.weather_info);
 
@@ -123,5 +112,12 @@ public class CitiesFragment extends Fragment {
         container.isTempOn = tempOn;
         container.isWindOn = windOn;
         return container;
+    }
+
+    @Override
+    public void onItemClicked(String itemText, int position) {
+        Toast.makeText(getContext(), itemText, Toast.LENGTH_SHORT).show();
+        currentPosition = position;
+        showWeatherDisp();
     }
 }
